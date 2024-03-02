b52f4793f3eebcf927ceb4d59cebb9934a029958
shraaavss <shravs2707@gmail.com>
2024-03-02 13:38:54 +0530

    shifted to vanilla

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,21 @@ import Appicon from '../assets/AppIcon.svg';
 import home from '../assets/homeIcon.svg';
 import search from '../assets/searchIcon.svg';
 import prof from '../assets/ProfileIcon.svg';
+import './Header.css';
 
 export default function Header() {
   return (
     <div>
-        <header className="flex p-8 justify-around">
+        <header className="main-wrap">
 
             <div className="app-logo">
-                <img src={Appicon} alt="app icon" />
+                <img className="nav-img" src={Appicon} alt="app icon" />
             </div>
 
-            <nav className="flex flex-row gap-10">
-                <img src={home} alt="home" />
-                <img src={search} alt="search" />
-                <img src={prof} alt="profile" />
+            <nav className="nav-bar">
+                <img className="nav-img" src={home} alt="home" />
+                <img className="nav-img" src={search} alt="search" />
+                <img className="nav-img" src={prof} alt="profile" />
             </nav>
 
         </header>
