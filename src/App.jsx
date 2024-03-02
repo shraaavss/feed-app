862cd7fdfe004fd0b51da4ed578788b2d24baa4f
shraaavss <shravs2707@gmail.com>
2024-03-02 13:13:30 +0530

    tailwind working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './output.css'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import icon from '../assets/yellowman.png'
+import '../output.css';
+import Appicon from '../assets/AppIcon.svg';
+import home from '../assets/homeIcon.svg';
+import search from '../assets/searchIcon.svg';
+import prof from '../assets/ProfileIcon.svg';
 
 export default function Header() {
   return (
     <div>
-        <header>
+        <header className="flex p-8">
             <div className="app-logo">
-                <img src={icon} alt="app icon" />
+                <img src={Appicon} alt="app icon" />
+                <img src={home} alt="app icon" />
+                <img src={search} alt="app icon" />
+                <img src={prof} alt="app icon" />
+                <p className="text-2xl font-bold">hello</p>
             </div>
         </header>
     </div>
