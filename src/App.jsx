033a6f00c6523c15772f8c63a01f3a0514a38a2e
snehguptafr <guptasneh302004@gmail.com>
2024-03-02 15:21:54 +0530

    add login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
+import Login from "./pages/Login"
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />}/>
       <Route path="/profile" element={<Profile />} />
       <Route path="/search" element={<Search />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     </BrowserRouter>
   )
