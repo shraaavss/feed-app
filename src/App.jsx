b65e11c1e28ab910613012ae516fef6acb87d7f7
snehguptafr <guptasneh302004@gmail.com>
2024-03-02 14:12:18 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
     </BrowserRouter>
   )
